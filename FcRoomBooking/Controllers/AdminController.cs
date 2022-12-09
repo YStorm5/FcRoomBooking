6a8579ceb4962ef2f252db125afd4dc4aee6aefd
YStorm5 <panhagt5@gmail.com>
2022-12-09 11:42:24 +0700

    Add more style

--- FcRoomBooking/Controllers/AdminController.cs
+++ FcRoomBooking/Controllers/AdminController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FcRoomBooking.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FcRoomBooking.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public AdminController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RoomBooking()
+        {
+            return View();
+        }
+        public async Task<IActionResult> CreateRoomBooking()
         {
             return View();
         }
