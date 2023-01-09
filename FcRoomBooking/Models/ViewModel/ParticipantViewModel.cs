f9c67687f10e0fb1184155fbfc0837856a3bbb54
YStorm5 <panhagt5@gmail.com>
2023-01-09 08:26:50 +0700

    Finish

--- FcRoomBooking/Models/ViewModel/ParticipantViewModel.cs
+++ FcRoomBooking/Models/ViewModel/ParticipantViewModel.cs
@@ -9,5 +9,7 @@ namespace FcRoomBooking.Models.ViewModel
         public string Username { get; set; }
         public string Email { get; set; }
         public string? userId { get; set; }
+        public string? Subject { get; set; }
+        public string? Detail { get; set; }
     }
 }
